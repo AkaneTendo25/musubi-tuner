f3d9ef31652a85255804d83454cc061915928a0e
AkaneTendo25 <seruva19@mail.ru>
2026-04-29 18:27:37 +0300

    add missing root scripts

---
+++ ltx2_mmcontrol_cache_latents.py
@@ -0,0 +1,5 @@
+from musubi_tuner.ltx2_mmcontrol_cache_latents import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ ltx2_mmcontrol_train.py
@@ -0,0 +1,5 @@
+from musubi_tuner.ltx2_mmcontrol_train import main
+
+
+if __name__ == "__main__":
+    main()
